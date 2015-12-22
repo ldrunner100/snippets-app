9ce2032cb87b7b0a2478bd41da7d630e7c721506
comason <comason@jackhenry.com>
2015-12-22 20:11:00 +0000

    subparsers added

--- snippets.py
+++ snippets.py
@@ -1,7 +1,27 @@
 import logging
+import argparse
+import sys
 
 #set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    
+    #subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+    
+    arguments = parser.parse_args(sys.argv[1:])
+    
+if __name__ == "__main__":
+    main()
 
 def put(name, snippet):
     """
@@ -22,5 +42,7 @@ def get(name):
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return name
+    
+
     
     
