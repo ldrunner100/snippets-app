d5757c9afdbac9326ab78ad0b780d51acb8e6f7c
comason <comason@jackhenry.com>
2015-12-22 20:20:35 +0000

    Add parser structure

--- snippets.py
+++ snippets.py
@@ -17,8 +17,21 @@ def main():
     put_parser = subparsers.add_parser("put", help="Store a snippet")
     put_parser.add_argument("name", help="The name of the snippet")
     put_parser.add_argument("snippet", help="The snippet text")
+    get_parser = subparsers.add_parser("get")
+    
     
     arguments = parser.parse_args(sys.argv[1:])
+    # convert parsed arguments from namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+    
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
+        
     
 if __name__ == "__main__":
     main()
